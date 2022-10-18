ee21e1208be73124876226d0fc4e851809a100c0
alfi <alfi@domainesia.com>
2022-10-19 05:03:35 +0700

    fix: remove unnecessary style and image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <ProductDisplay/>
 </template>
 
@@ -13,14 +12,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
