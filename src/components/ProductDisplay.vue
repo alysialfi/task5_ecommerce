5833473299b6a61122304068134266eb36338711
alfi <alfi@domainesia.com>
2022-10-19 05:04:59 +0700

    feat: styling ProductDisplay component

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,6 +1,39 @@
 <template>
-    <div>
-        <h1>Tes halaman produk</h1>
+    <div class="container">
+        <div class="product-container">
+            <div class="product-thumbnail">
+                <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt="Mens Casual Premium Slim Fit T-Shirts thumbnail">
+            </div>
+            <div class="product-details">
+                <div>
+                    <h3 class="title">Lock and Love Women's Removable Hooded
+                        Faux Leather Moto Biker Jacket</h3>
+                    <div class="sub-title">
+                        <span>men's clothing</span>
+                        <div class="rating">
+                            <span>2.9/5</span>
+                            <div class="rating">
+                                <span class="circle"></span>
+                                <span class="circle"></span>
+                                <span class="circle"></span>
+                                <span class="circle"></span>
+                                <span class="circle"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="description">
+                        <p>Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.</p>
+                    </div>
+                </div>
+                <div>
+                    <span class="price">$29.95</span>
+                    <div class="cta">
+                        <button type="button" class="cta-buy">Buy Now</button>
+                        <button type="button" class="cta-next">Next Product</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,3 +47,7 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+    @import '../assets/style/page.css'
+</style>
