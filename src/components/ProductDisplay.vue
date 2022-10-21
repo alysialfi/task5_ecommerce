ccbb696c04bc6302e56f1c172fb1fe9ba7bb84a9
alfi <alfi@domainesia.com>
2022-10-21 11:25:09 +0700

    fix: layout and styling

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,48 +1,60 @@
 <template>
     <div class="container">
-        <div v-if="isLoading" class="product-container">
-            <div class="skeleton-thumbnail"></div>
-            <div class="skeleton-details">
-                <div class="skeleton-details-top"></div>
-                <div class="skeleton-details-bottom"></div>
+        <div v-if="isLoading" class="card">
+            <div class="product-container">
+                <div class="skeleton-thumbnail"></div>
+                <div class="skeleton-details">
+                    <div class="skeleton-details-top"></div>
+                    <div class="skeleton-details-bottom"></div>
+                </div>
             </div>
         </div>
         <div v-else class="container" :class="!isProductAvailable ? 'bg-gray' : product.data.category === 'men\'s clothing' ? 'bg-blue' : 'bg-pink'">
-            <div v-if="!isProductAvailable" class="product-unavailable-container">
-                <p>This product is unavailable to show</p>
-                <div class="cta">
-                    <button type="button" @click="getSingleProduct()" class="cta-next">Next Product</button>
-                </div>
+            <div class="overlay">
+                <img src="../assets/bg-shape.svg" alt="background overlay">
             </div>
-            <div v-else class="product-container">
-                <div class="product-thumbnail">
-                    <img :src="product.data.image" alt="">
-                </div>
-                <div class="product-details">
-                    <div class="top">
-                        <h3 :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="title">{{ product.data.title }}</h3>
-                        <div class="sub-title">
-                            <span>{{ product.data.category }}</span>
-                            <div class="rating">
-                                <span>{{ product.data.rating.rate }}/5</span>
-                                <div class="rating">
-                                    <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
-                                    <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
-                                    <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
-                                    <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
-                                    <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="description">
-                            <p>{{ product.data.description }}</p>
+            <div class="card">
+                <div v-if="!isProductAvailable" class="product-unavailable-container">
+                    <div class="overlay">
+                        <img src="../assets/bg-sad.svg" alt="background unavailable product" srcset="">
+                    </div>
+                    <div class="product-details">
+                        <p>This product is unavailable to show</p>
+                        <div class="cta">
+                            <button type="button" @click="getSingleProduct()" class="cta-next">Next Product</button>
                         </div>
                     </div>
-                    <div class="bottom">
-                        <span :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="price">${{ product.data.price }}</span>
-                        <div class="cta">
-                            <button type="button" :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="cta-buy">Buy Now</button>
-                            <button type="button" @click="getSingleProduct()" :class="product.data.category === 'men\'s clothing' ? 'border-navy font-navy' : 'border-magenta font-magenta'" class="cta-next">Next Product</button>
+                </div>
+                <div v-else class="product-container">
+                    <div class="product-thumbnail">
+                        <img :src="product.data.image" alt="">
+                    </div>
+                    <div class="product-details">
+                        <div class="top">
+                            <h3 :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="title">{{ product.data.title }}</h3>
+                            <div class="sub-title">
+                                <span>{{ product.data.category }}</span>
+                                <div class="rating">
+                                    <span>{{ product.data.rating.rate }}/5</span>
+                                    <div class="rating">
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="description">
+                                <p>{{ product.data.description }}</p>
+                            </div>
+                        </div>
+                        <div class="bottom">
+                            <span :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="price">${{ product.data.price }}</span>
+                            <div class="cta">
+                                <button type="button" :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="cta-buy">Buy Now</button>
+                                <button type="button" @click="getSingleProduct()" :class="product.data.category === 'men\'s clothing' ? 'border-navy font-navy' : 'border-magenta font-magenta'" class="cta-next">Next Product</button>
+                            </div>
                         </div>
                     </div>
                 </div>
